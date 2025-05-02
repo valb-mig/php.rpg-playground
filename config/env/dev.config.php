c95d8a33efcac6649df17c841d45b90f858e777d
valb-mig <valb.mig@gmail.com>
2025-05-01 21:06:40 -0300

    🔧(chore): Removing game master entity + useCases + Repository

--- config/env/dev.config.php
+++ config/env/dev.config.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 return [
     'repository' => [
-        'game_master' => Rpg\Infrastructure\Repository\Fake\FakeGameMasterRepository::class,
         'character'   => Rpg\Infrastructure\Repository\Fake\FakeCharacterRepository::class,
         'item'        => Rpg\Infrastructure\Repository\Fake\FakeItemRepository::class,
         'user'        => Rpg\Infrastructure\Repository\Fake\FakeUserRepository::class
--- config/env/prod.config.php
+++ config/env/prod.config.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 return [
     'repository' => [
-        'game_master' => throw new \Exception('Not implemented'),
         'character'   => throw new \Exception('Not implemented'),
         'item'        => throw new \Exception('Not implemented'),
         'user'        => throw new \Exception('Not implemented')
