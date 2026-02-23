2add3258a6f4ec245e8346164b5a8451cf5b941d
valb-mig <valb.mig@gmail.com>
2026-02-23 18:02:56 -0300

    (dump)

---
+++ tests/Domain/Entities/DiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Domain\Entities;
+
+use Domain\Entities\Dice;
+use PHPUnit\Framework\TestCase;
+
+class DiceTest extends TestCase
+{
+    public function testRoll()
+    {
+    }
+}
