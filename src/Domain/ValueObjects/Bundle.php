4610a3ba3609559826c9118089894594d6ebaf30
valb-mig <valb.mig@gmail.com>
2026-03-25 18:36:40 -0300

    feat: (Bundle) add generic immutable bundle base value object

---
+++ src/Domain/ValueObjects/Bundle.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RPGPlayground\Domain\ValueObjects;
+
+/**
+ * @template TData
+ */
+abstract class Bundle
+{
+    /**
+     * @param array<string, TData> $bundleData
+     */
+    public function __construct(
+        public readonly array $bundleData = [],
+    ) {}
+
+    /**
+     * @param array<string, TData> $bundleData
+     * @return static
+     */
+    public function add(array $bundleData): static
+    {
+        return new static(array_merge($this->bundleData, $bundleData));
+    }
+
+    /**
+     * @param string $key
+     * @return static
+     */
+    public function remove(string $key): static
+    {
+        return new static(array_diff_key($this->bundleData, [$key => true]));
+    }
+
+    /**
+     * @param string $key
+     * @return TData|null
+     */
+    public function get(string $key): mixed
+    {
+        return $this->has($key) ? $this->bundleData[$key] : null;
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->bundleData);
+    }
+}
