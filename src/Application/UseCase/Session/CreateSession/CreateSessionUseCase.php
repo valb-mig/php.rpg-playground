6205db3d363e1e63663b2c89c218bfc3ceaf8512
valb-mig <valb.mig@gmail.com>
2026-04-20 17:06:06 -0300

    feat(Application/UseCase/Session/CreateSession): add session creation event handling
    - dispatch `CreatedSessionEvent` after creating a new session
    - update use case to include `RPGSessionEventHandler`

--- src/Application/UseCase/Session/CreateSession/CreateSessionUseCase.php
+++ src/Application/UseCase/Session/CreateSession/CreateSessionUseCase.php
@@ -6,9 +6,11 @@ namespace RPGKernel\Application\UseCase\Session\CreateSession;
 
 use Eco\Error;
 use Eco\Result;
+use RPGKernel\Core\Handler\RPGSessionEventHandler;
 use RPGKernel\Core\Utils\Identifier;
 use RPGKernel\Domain\Entities\Session;
 use RPGKernel\Domain\Enums\SessionStatus;
+use RPGKernel\Domain\Events\Session\CreatedSessionEvent;
 
 final class CreateSessionUseCase
 {
@@ -21,6 +23,9 @@ final class CreateSessionUseCase
         try {
             $session = new Session(identifier: Identifier::generate(), name: $input->name, createdAt: new \DateTime());
             $session->setStatus(SessionStatus::CREATED);
+
+            RPGSessionEventHandler::dispatch(new CreatedSessionEvent($session));
+
             return Result::ok(new CreateSessionOutput(session: $session));
         } catch (\Exception $e) {
             return Result::fail(Error::generic($e->getMessage()));
